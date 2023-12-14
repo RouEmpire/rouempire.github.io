1b214b11154c483d63c8849d5f999848126f16c1
RouEmpire <rouempire1446@gmail.com>
2023-12-14 17:34:44 +0700

    trying new back button 	-add JavaScript

--- ref/script.js
+++ ref/script.js
@@ -1,3 +1,9 @@
 function ToCertificate() {
     window.location.href = "certificate.html";
 }
+function toAboutme() {
+    window.location.href = "aboutme.html";
+}
+function returnHome() {
+    window.location.href = "../index.html";
+}
